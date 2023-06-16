217a365343fd87391e24e23e754ee530918cbdbe
dawid <dawid@dawids-MacBook-Air.local>
2023-06-16 17:35:10 +0200

    add static files directory and connect them with fastapi

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 router = APIRouter()
+
+router.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 @router.get("/register")
 async def register_get():
